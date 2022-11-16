faf977fa08a02ffab15df0b3abe3fa24e565ab19
Randy <randomzakya@gmail.com>
2022-11-17 05:28:14 +0700

    add cuda visible devices

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-python main.py --model_name_or_path hustvl/yolos-tiny --data_dir dataset_masks \
+CUDA_VISIBLE_DEVICES=0 python main.py --model_name_or_path hustvl/yolos-tiny --data_dir dataset_masks \
                             --train train --val eval --test test \
                             --do_train --do_eval --do_predict \
                             --train_args train_args.json --random_seed 42
