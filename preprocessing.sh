0f9ff1b40f39144fbcecd16354f13f02c759387b
Randy <randomzakya@gmail.com>
2022-11-16 16:06:54 +0700

    edit shell script for preprocessing

--- preprocessing.sh
+++ preprocessing.sh
@@ -2,4 +2,4 @@ input_dir = "./dataset_masks/train/Annotations"
 image_dir = "./dataset_masks/train/Images"
 output_dir = "./dataset_masks/train/Labels"
 
-python preprocessing.py --input $input_dir --image $image_dir --output $output_dir
+python preprocessing.py --input ${input_dir} --image ${image_dir} --output ${output_dir}
