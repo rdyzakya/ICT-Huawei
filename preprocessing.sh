2367b042c548eccaad80eb8f20c9944eec9aa684
Randy <randomzakya@gmail.com>
2022-11-16 16:07:58 +0700

    rename _class.txt

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,5 +1,7 @@
-input_dir = "./dataset_masks/train/Annotations"
-image_dir = "./dataset_masks/train/Images"
-output_dir = "./dataset_masks/train/Labels"
+#!/usr/bin/env bash
+
+input_dir="./dataset_masks/train/Annotations"
+image_dir="./dataset_masks/train/Images"
+output_dir="./dataset_masks/train/Labels"
 
 python preprocessing.py --input ${input_dir} --image ${image_dir} --output ${output_dir}
