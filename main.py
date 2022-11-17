fad5af7b630f887926de2ef5165de53d96b7484a
Randy <randomzakya@gmail.com>
2022-11-17 07:46:56 +0700

    make native train loop

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import argparse
-import torch
 # from torchvision.ops import box_convert
 # from torchvision.utils import draw_bounding_boxes
-from torchvision.transforms.functional import pil_to_tensor, to_pil_image
+# from torchvision.transforms.functional import pil_to_tensor, to_pil_image
 
+import torch
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
 import transformers
 from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
+from transformers import get_scheduler
+import evaluate
 
 import datasets
-from PIL import Image
+from preprocess_dataset import read_dataset, coco_format_annotation
 
 import os
 import json
 
-from preprocess_dataset import read_dataset, coco_format_annotation
+from tqdm.auto import tqdm
 
 # https://huggingface.co/docs/datasets/object_detection
 
@@ -25,6 +29,8 @@ def init_args():
     parser.add_argument("--train", type=str, default="train", help="Path to train folder")
     parser.add_argument("--val", type=str, default="val", help="Path to val folder")
     parser.add_argument("--test", type=str, default="test", help="Path to test folder")
+
+    parser.add_argument("--n_gpu", type=str, default="0", help="Index of GPU to use")
 
     parser.add_argument("--do_train", action="store_true", help="Train the model")
     parser.add_argument("--do_eval", action="store_true", help="Evaluate the model")
@@ -38,33 +44,89 @@ def init_args():
     return args
 
 def train(args,model,feature_extractor,dataset,annotations,train_args):
+    torch.cuda.empty_cache()
     # For native pt : https://huggingface.co/docs/transformers/training#train-in-native-pytorch
+    inputs = {}
+    dataloaders = {}
 
     # Feature extract the dataset
-    # tensors = [pil_to_tensor(image) for image in dataset["train"]["image"]]
-    inputs_train = feature_extractor(images=dataset["train"]["image"], annotations=annotations["train"], return_tensors="pt")
-    # Prepare the training arguments
+    inputs["train"] = feature_extractor(images=dataset["train"]["image"], annotations=annotations["train"], return_tensors="pt")
+    dataloaders["train"] = DataLoader(inputs["train"], shuffle=False, batch_size=train_args["per_device_train_batch_size"])
 
-    # For reference : https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
-    training_arguments = transformers.TrainingArguments(**train_args)
-    trainer_args = {
-        "model": model,
-        "args": training_arguments,
-        "train_dataset": inputs_train,
-        "tokenizer": feature_extractor,
-        "do_train": args.do_train,
-        "do_eval": args.do_eval,
-        "do_predict": args.do_predict,
-    }
-    # If evaluation then...
     if args.do_eval:
-        inputs_val = feature_extractor(images=dataset["val"]["image"], annotations=annotations["val"], return_tensors="pt")
-        trainer_args["eval_dataset"] = inputs_val
-    # Prepare the trainer
-    trainer = transformers.Trainer(**trainer_args)
-    # Train the model
-    print("Start the training...")
-    trainer.train()
+        inputs["val"] = feature_extractor(images=dataset["val"]["image"], annotations=annotations["val"], return_tensors="pt")
+        dataloaders["val"] = DataLoader(inputs["val"], shuffle=False, batch_size=train_args["per_device_eval_batch_size"])
+
+    lr = train_args["learning_rate"] or 5e-5
+    adam_epsilon = train_args["adam_epsilon"] or 1e-8
+    weight_decay = train_args["weight_decay"] or 0.0
+    betas = (train_args["adam_beta1"] or 0.9, train_args["adam_beta2"] or 0.999)
+    optimizer = AdamW(model.parameters() , lr=lr, eps=adam_epsilon, weight_decay=weight_decay, betas=betas)
+
+    num_epochs = train_args["num_train_epochs"] or 3
+    num_training_steps = num_epochs * len(dataloaders["train"])
+    num_warmup_steps = train_args["warmup_steps"] or 0
+    lr_scheduler = get_scheduler(
+        name="linear", optimizer=optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps
+    )
+
+    device = torch.device(f"cuda:{args.n_gpu}" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    print("Start training...")
+    print("Training arguments:")
+    print(train_args)
+    progress_bar = tqdm(range(num_training_steps))
+
+    for epoch in range(num_epochs):
+        model.train()
+        for batch in dataloaders["train"]:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            loss = outputs.loss
+            loss.backward()
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+        
+        if args.do_eval:
+            pass
+            # model.eval()
+            # metric = evaluate.load("accuracy")
+            # model.eval()
+            # for batch in dataloaders["val"]:
+            #     batch = {k: v.to(device) for k, v in batch.items()}
+            #     with torch.no_grad():
+            #         outputs = model(**batch)
+
+            #     logits = outputs.logits
+            #     predictions = torch.argmax(logits, dim=-1)
+            #     metric.add_batch(predictions=predictions, references=batch["labels"])
+    # Prepare the training arguments
+    # train_args.update({
+    #     "do_train": args.do_train,
+    #     "do_eval": args.do_eval,
+    #     "do_predict": args.do_predict,
+    # })
+    # For reference : https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
+    # training_arguments = transformers.TrainingArguments(**train_args)
+    # trainer_args = {
+    #     "model": model,
+    #     "args": training_arguments,
+    #     "train_dataset": inputs_train,
+    #     "tokenizer": feature_extractor,
+    # }
+    # # If evaluation then...
+    # if args.do_eval:
+    #     inputs_val = feature_extractor(images=dataset["val"]["image"], annotations=annotations["val"], return_tensors="pt")
+    #     trainer_args["eval_dataset"] = inputs_val
+    # # Prepare the trainer
+    # trainer = transformers.Trainer(**trainer_args)
+    # # Train the model
+    # print("Start the training...")
+    # trainer.train()
 
 def main():
     args = init_args()
