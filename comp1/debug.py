06d2dfa820b6489764421ec2a1c3a61aa6715501
rdyzakya <randomzakya@gmail.com>
2023-04-27 07:33:02 +0700

    create: add for productivity video ; delete: delete unnecessary folder and files

--- comp1/debug.py
+++
@@ -1,9 +0,0 @@
-from preprocess_dataset import read_dataset, coco_format_annotation, map_coco_annotation
-
-image_path = "./dataset_masks/train/Images"
-label_path = "./dataset_masks/train/Labels"
-
-ds = read_dataset(image_path, label_path, format="coco")
-
-tes = ds.map(map_coco_annotation, batched=False, remove_columns=ds.column_names)
-print(tes[0])
--- comp1/preprocess_xml.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-input_dir="./dataset_masks/test/Annotations"
-image_dir="./dataset_masks/test/Images"
-output_dir="./dataset_masks/test/Labels"
-
-python preprocess_xml.py --input ${input_dir} --image ${image_dir} --output ${output_dir}
